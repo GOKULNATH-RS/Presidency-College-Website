6ee5dadcb5c8146b96172670100d4ecd528847d1
gokulnathpeace@gmail.com <gokulnathpeace@gmail.com>
2024-06-13 13:33:59 +0530

    Project cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Hero from "./sections/Hero";
-import Rank from "./sections/Rank";
-import Video from "./sections/Video";
-import Scholarship from "./sections/Scholarship";
-import Programme from "./sections/Programme";
-import BSchool from "./sections/BSchool";
-import Testimontials from "./sections/Testimontials";
-import CampusLife from "./sections/CampusLife";
-import Blog from "./sections/Blog";
-import Footer from "./components/Footer";
-import AdmissionForm from "./components/Application_form";
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Hero from './sections/Hero'
+import Rank from './sections/Rank'
+import Video from './sections/Video'
+import Scholarship from './sections/Scholarship'
+import Programme from './sections/Programme'
+import BSchool from './sections/BSchool'
+import Testimontials from './sections/Testimontials'
+import CampusLife from './sections/CampusLife'
+import Blog from './sections/Blog'
+import Footer from './components/Footer'
+import AdmissionForm from './components/Application_form'
 
 const App = () => {
-  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [isFormVisible, setIsFormVisible] = useState(false)
 
   const handleApplyNowClick = () => {
-    setIsFormVisible(true);
-  };
+    setIsFormVisible(true)
+  }
 
   const handleFormClose = () => {
-    setIsFormVisible(false);
-  };
+    setIsFormVisible(false)
+  }
 
   return (
-    <div className="bg-[#FFF6EA] p-0 m-0 overflow-hidden">
+    <div className='bg-[#FFF6EA] p-0 m-0 overflow-hidden'>
       <Router>
         <Navbar onApplyNowClick={handleApplyNowClick} />
         <Routes>
           <Route
-            path="/"
+            path='/'
             element={
-              <div className="p-0 m-0 overflow-hidden ">
+              <div className='p-0 m-0 overflow-hidden '>
                 <Hero onApplyNowClick={handleApplyNowClick} />
                 <Rank />
                 <Video />
@@ -50,7 +50,7 @@ const App = () => {
         <AdmissionForm isVisible={isFormVisible} onClose={handleFormClose} />
       </Router>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
